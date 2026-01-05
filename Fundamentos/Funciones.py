d926d6f542785bad6329f9cec1f13362774f1b76
Daniela Chanchí Galindo <danielachanchi.github@gmail.com>
2026-01-05 18:31:19 -0500

    Algoritmos de funciones modificados a SmartMathPy

--- Fundamentos/Funciones.py
+++ Fundamentos/Funciones.py
@@ -1,18 +1,16 @@
 from utils import convertir_valores
-
-funcion = input("Ingrese la función a evaluar en términos de a con espacios entre cada término: ").split()
-
-cantidad = int(input("Ingrese la cantidad de imágenes que desea encontrar: "))
-
     
-def evaluar_funcion(f, d):
-    print(f, d)
+def evaluar_funcion(function: list, domains: list):
+    """
+    The function evaluates a mathematical expression with variables replaced by values from a given
+    domain.
+    """
 
     resultados = {}
 
-    for elemento in d:
+    for elemento in domains:
         nueva_funcion = []
-        for token in f: # La función la tengo con split y eso devuelve tokens, por eso evaluo cada token
+        for token in function: # La función la tengo con split y eso devuelve tokens, por eso evaluo cada token
             if token == "a":
                 nueva_funcion.append(elemento)
             elif token in ["+", "-", "*", "**", "(", ")", "/"]:
@@ -53,14 +51,54 @@ def evaluar_funcion(f, d):
                 print("Resta resuelta: ", resultado)
     
         resultados[elemento] = nueva_funcion[0]
+
     return resultados
 
-dominios = []
-for c in range(cantidad):
-    valor = input(f"Ingrese el {c+1} dominio a encontrar: ")
-    dominio = convertir_valores(valor)
-    dominios.append(dominio)
+def clasificacion(A: set, B: set, function):
 
-respuesta = evaluar_funcion(funcion, dominios)
+    """
+    The function clasificacion determines if a given function is injective, surjective, and bijective
+    based on sets A and B and the function provided.
+    """
 
-print("Solución final: ", respuesta)
+    imagen = []
+    inyectiva = True
+    sobreyectiva = True
+    biyectiva = True
+
+    for valor in A:
+        a = valor #toma el valor del conjunto que corresponde a la iteración
+        resultado_funcion = eval(function) # Evalua la función que digitó el usuario
+        imagen.append(resultado_funcion) 
+
+    print("Imagen =", imagen)
+
+    # This part of the code is checking whether the function is injective or not. Here's a breakdown
+    # of what it's doing:
+
+    set_imagen = set(imagen) 
+
+    if len(imagen) == len(set_imagen): #Si el set no eliminó nada es porque no habían repeticiones, condición de la inyectiva
+        inyectiva = True
+    else:
+         inyectiva = False
+
+    # This part of the code is checking whether the function is surjective (also known as onto) or
+    # not. Here's a breakdown of what it's doing:
+
+    auxiliar = []
+    auxiliar = set(B).difference(set(imagen)) 
+    if len(auxiliar) != 0: 
+        sobreyectiva = False
+    else:
+        sobreyectiva = True
+
+    # The code snippet `if inyectiva == True and sobreyectiva == True:` is checking if both the
+    # conditions for a function to be bijective are met.
+    
+    if inyectiva == True and sobreyectiva == True:
+        biyectiva = True
+    else:
+        biyectiva = False
+        
+    return f"Inyectiva {inyectiva}, sobreyectiva {sobreyectiva} y biyectiva {biyectiva}"
