6d63ac4e1800b25973d59c88a248b00f4870f2fb
Daniela Chanchí Galindo <danielachanchi.github@gmail.com>
2026-01-07 17:35:39 -0500

    Traducción al inglés de Funciones.py

--- Fundamentos/Funciones.py
+++ Fundamentos/Funciones.py
@@ -1,104 +1,104 @@
 from utils import convertir_valores
     
-def evaluar_funcion(function: list, domains: list):
+def evaluate_function(function: list, domains: list):
     """
     The function evaluates a mathematical expression with variables replaced by values from a given
     domain.
     """
-
-    resultados = {}
-
-    for elemento in domains:
-        nueva_funcion = []
-        for token in function: # La función la tengo con split y eso devuelve tokens, por eso evaluo cada token
-            if token == "a":
-                nueva_funcion.append(elemento)
-            elif token in ["+", "-", "*", "**", "(", ")", "/"]:
-                nueva_funcion.append(token)
-            else:
-                funcion_convertida = convertir_valores(token)
-                nueva_funcion.append(funcion_convertida)
-        print(f"Función reemplazando a por {elemento}: ",nueva_funcion)
-
-        while "**" in nueva_funcion:
-            i = nueva_funcion.index("**")
-            resultado = nueva_funcion[i-1] ** nueva_funcion[i+1]
-            nueva_funcion[i-1:i+2] = [resultado]
-            print("Potencia resuelta: ",resultado)
-
-        while "*" in nueva_funcion or "/" in nueva_funcion:
-            if "*" in nueva_funcion:
-                i = nueva_funcion.index("*")
-                resultado = nueva_funcion[i-1] * nueva_funcion[i+1]
-                nueva_funcion[i-1:i+2] = [resultado]
-                print("Multiplicación resuelta: ",resultado)
-            else:
-                i = nueva_funcion.index("/")
-                resultado = nueva_funcion[i-1] / nueva_funcion[i+1]
-                nueva_funcion[i-1:i+2] = [resultado]
-                print("División resuelta: ",resultado)
-
-        while "+" in nueva_funcion or "-" in nueva_funcion:
-            if "+" in nueva_funcion:
-                i = nueva_funcion.index("+")
-                resultado = nueva_funcion[i-1] + nueva_funcion[i+1]
-                nueva_funcion[i-1:i+2] = [resultado]
-                print("Suma resuelta: ",resultado)
-            else:
-                i = nueva_funcion.index("-")
-                resultado = nueva_funcion[i-1] - nueva_funcion[i+1]
-                nueva_funcion[i-1:i+2] = [resultado]
-                print("Resta resuelta: ", resultado)
     
-        resultados[elemento] = nueva_funcion[0]
 
-    return resultados
+    results = {}
 
-def clasificacion(A: set, B: set, function):
+    for element in domains:
+        new_function = []
+        for token in function: 
+            if token == "a":
+                new_function.append(element)
+            elif token in ["+", "-", "*", "**", "(", ")", "/"]:
+                new_function.append(token)
+            else:
+                converted_function = convertir_valores(token)
+                new_function.append(converted_function)
+        print(f"Función reemplazando a por {element}: ",new_function)
 
+        while "**" in new_function:
+            i = new_function.index("**")
+            result = new_function[i-1] ** new_function[i+1]
+            new_function[i-1:i+2] = [result]
+            print("Potencia resuelta: ",result)
+
+        while "*" in new_function or "/" in new_function:
+            if "*" in new_function:
+                i = new_function.index("*")
+                result = new_function[i-1] * new_function[i+1]
+                new_function[i-1:i+2] = [result]
+                print("Multiplicación resuelta: ",result)
+            else:
+                i = new_function.index("/")
+                result = new_function[i-1] / new_function[i+1]
+                new_function[i-1:i+2] = [result]
+                print("División resuelta: ",result)
+
+        while "+" in new_function or "-" in new_function:
+            if "+" in new_function:
+                i = new_function.index("+")
+                result = new_function[i-1] + new_function[i+1]
+                new_function[i-1:i+2] = [result]
+                print("Suma resuelta: ",result)
+            else:
+                i = new_function.index("-")
+                result = new_function[i-1] - new_function[i+1]
+                new_function[i-1:i+2] = [result]
+                print("Resta resuelta: ", result)
+    
+        results[element] = new_function[0]
+
+    return results
+
+def classification(A: set, B: set, function):
     """
-    The function clasificacion determines if a given function is injective, surjective, and bijective
-    based on sets A and B and the function provided.
+    The function takes two sets A and B, along with a function, and determines if the function is
+    injective, surjective, or bijective.
     """
 
-    imagen = []
-    inyectiva = True
-    sobreyectiva = True
-    biyectiva = True
+    image = []
+    injective = True
+    surjective = True
+    bijective = True
 
-    for valor in A:
-        a = valor #toma el valor del conjunto que corresponde a la iteración
-        resultado_funcion = eval(function) # Evalua la función que digitó el usuario
-        imagen.append(resultado_funcion) 
+    for value in A:
+        a = value
+        classification = eval(function)
+        image.append(classification) 
 
-    print("Imagen =", imagen)
+    print("Image =", image)
 
     # This part of the code is checking whether the function is injective or not. Here's a breakdown
     # of what it's doing:
 
-    set_imagen = set(imagen) 
+    set_image = set(image) 
 
-    if len(imagen) == len(set_imagen): #Si el set no eliminó nada es porque no habían repeticiones, condición de la inyectiva
-        inyectiva = True
+    if len(image) == len(set_image): #Si el set no eliminó nada es porque no habían repeticiones, condición de la inyectiva
+        injective = True
     else:
-         inyectiva = False
+         injective = False
 
     # This part of the code is checking whether the function is surjective (also known as onto) or
     # not. Here's a breakdown of what it's doing:
 
-    auxiliar = []
-    auxiliar = set(B).difference(set(imagen)) 
-    if len(auxiliar) != 0: 
-        sobreyectiva = False
+    auxiliary = []
+    auxiliary = set(B).difference(set(image)) 
+    if len(auxiliary) != 0: 
+        surjective = False
     else:
-        sobreyectiva = True
+        surjective = True
 
-    # The code snippet `if inyectiva == True and sobreyectiva == True:` is checking if both the
+    # The code snippet `if injective == True and surjective == True:` is checking if both the
     # conditions for a function to be bijective are met.
     
-    if inyectiva == True and sobreyectiva == True:
-        biyectiva = True
+    if injective == True and surjective == True:
+        bijective = True
     else:
-        biyectiva = False
+        bijective = False
         
-    return f"Inyectiva {inyectiva}, sobreyectiva {sobreyectiva} y biyectiva {biyectiva}"
+    return f"Injective {injective}, surjective {surjective} y bijective {bijective}"
