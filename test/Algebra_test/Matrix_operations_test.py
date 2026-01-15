f331f13706b90feb33a2294d4431e644f4663a49
Daniela Chanchí Galindo <danielachanchi.github@gmail.com>
2026-01-14 19:15:48 -0500

    matrix_sum test

---
+++ test/Algebra_test/Matrix_operations_test.py
@@ -0,0 +1,35 @@
+from Algebra.Matrix_folder.Matrix_operations import matrix_sum, matrix_subtraction, scalar_multiply_matrix
+
+# py -m test.Algebra_test.Matrix_operations_test
+
+print("-----------matrix_sum_test-------------")
+
+def matrix_sum_test():
+    A = [[4,2],
+         [2,4]]
+    
+    B = [[3,6],
+         [6,3]]
+    
+    result = matrix_sum(A, B)
+
+    for row in result:
+        for element in row:
+            print(element, end=" ")
+        print()
+
+    # ValueError case
+    C = [[1,2],
+         [3,4]]
+    
+    D = [[1,2,3],
+         [4,5,6]]
+
+    try:
+        result = matrix_sum(C, D)
+        print("ERROR: debía fallar y no falló")
+    except ValueError:
+        print("matrix_sum: The arrays must have the same number of rows and columns")
+
+matrix_sum_test()
+
