8a3ced3aeb77451e85a73a2bbd90929d61dd4955
Daniela Chanchí Galindo <danielachanchi.github@gmail.com>
2026-01-14 19:25:32 -0500

    scalar_multiply_test

--- test/Algebra_test/Matrix_operations_test.py
+++ test/Algebra_test/Matrix_operations_test.py
@@ -64,3 +64,31 @@ def matrix_subtraction_test():
         print("matrix_sum: The arrays must have the same number of rows and columns")
 
 matrix_subtraction_test()
+
+print("-----------scalar_multiply_test-------------")
+
+def scalar_multiply_test():
+    A = [[2,2],
+         [2,2]]
+    
+    scalar = 1
+
+    result = scalar_multiply_matrix(A, scalar)
+
+    for row in result:
+        for element in row:
+            print(element, end=" ")
+        print()
+
+    # 0 matrix
+
+    scalar = 0
+
+    result = scalar_multiply_matrix(A, scalar)
+
+    for row in result:
+        for element in row:
+            print(element, end=" ")
+        print()
+
+scalar_multiply_test()
